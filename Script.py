8cbb9c97fff3a55d42363ed741d3bf9c6cb6bc09
ROYAL BOTS <140387771+RoyalCreations07@users.noreply.github.com>
2023-07-25 02:08:58 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -8,12 +8,12 @@ class script(object):
 <b>✦ ɪᴛs ᴇᴀsʏ ᴛᴏ ᴜsᴇ ᴍᴇ. ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ</b>"""
 
     MY_ABOUT_TXT = """<b>✯ ᴍᴀɪɴ ʙᴏᴛ:  <a href=https://t.me/BotTest_MsBot>ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ</a></b>
-<b>✯ Cʀᴇᴀᴛᴏʀ:  <a href=http://t.me/Mahi_458>ᴍᴀʜᴇꜱʜ.ꜱ</a></b>
-<b>✯ ᴍᴏᴠɪᴇꜱ: <a href=https://t.me/+fQW_IELD_mhiMGY9>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
-<b>✯ ᴜᴩᴅᴀᴛᴇꜱ:  <a href=https://t.me/+fQW_IELD_mhiMGY9>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>"""
+<b>✯ Cʀᴇᴀᴛᴏʀ:  <a href=http://t.me/yashu1515>YASHU.R</a></b>
+<b>✯ ᴍᴏᴠɪᴇꜱ: <a href=https://t.me/+V4PzBSqfIXg2Njk9>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
+<b>✯ ᴜᴩᴅᴀᴛᴇꜱ:  <a href=https://t.me/Kannada_Pirates>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>"""
 
-    MY_OWNER_TXT = """<b>★ Name: ᴍᴀʜᴇꜱʜ.ꜱ</b>
-<b>★ Username: @MaHi_458</b>
+    MY_OWNER_TXT = """<b>★ Name: YASHU.R</b>
+<b>★ Username: @yashu1515</b>
 <b>★ ID:</b> <code>5450011131</code>
 <b>★ Country: India</b>"""
 
